2be4a00f1cbe41da242b811bb0f71ada26c34fab
SboneloMdluli <1101772@students.wits.ac.za>
2019-02-21 20:37:33 +0200

    3D matrx presentation

--- lab1.py
+++ lab1.py
@@ -1,5 +1,9 @@
 import random
 
+def Print_MAT(MAT):
+    for x in MAT:
+        print(x)
+
 def exctract_clmn(b,j):
     return [row[j] for row in b]
     
@@ -19,7 +23,11 @@ def rowvec_to_mat(vec_b):
     
     return rol_matrix
     
-def rank3d(a,b):
+def rank3TensorAdd(A,val,x,i,k):
+    A[x][i][k]+=val
+    
+    
+def rank3TensorMult(a,b):
 
     h_plane = [[0 for x in range(len(a))] for y in range(len(a))]
     
@@ -27,20 +35,14 @@ def rank3d(a,b):
         
     mat_a = mat_b = [[0 for x in range(len(a))] for y in range(len(a))]
     
-    
-    rank3_C[0][1][1] =0
     tmp=rslt_ =0
-    
-    print(rank3_C)
-    
-    
-
+        
     for i in range(len(a)): 
         h_plane = a[i]# select horizontal plane in a
-        #print(h_plane,'hp')
+       
         for j in range(len(b)):
             v_plane = exctract_clmn(b,j) # select vertical plane in b
-         #   print(v_plane,'vp')
+
             
             for k in range(len(h_plane)): #go through rows in horizontal plane
                 
@@ -48,24 +50,18 @@ def rank3d(a,b):
                    
                     Vplane_clm = exctract_clmn(v_plane,x) 
                     
-                    print(h_plane[k],Vplane_clm,'row & col')
-                    print('------------------------------')
+                    # must convert vector to n*n matrices
                     mat_a =rowvec_to_mat(h_plane[k])
                     
                     mat_b =colvec_to_mat(Vplane_clm)
                     
-                    print(mat_a,'rowM')
-                    print('------------------------------')
-                    print(mat_b,'colM')
+
+                    tmp=rank2TensorMult(mat_a,mat_b) 
+                    rslt_ = tmp[0][0] # get single value
                     
-                    tmp=rank2TensorMult(mat_a,mat_b)
-                    rslt_ = tmp[0][0]
-                    print('------------------------------')      
-                    print(rslt_,'single value')
-                    rank3_C[x][i][k]=rslt_
-                    print('-------------ANS-----------------')   
-                    print(rank3_C)
-                    print('-------------ANS-----------------')  
+                    rank3TensorAdd(rank3_C,rslt_,x,i,k)
+                   
+
     return rank3_C
 
 def rank2TensorAdd(a,b,c,i,j):
@@ -95,32 +91,15 @@ def check_dimensions(a,b):
 
 #main program 
 if __name__ == "__main__":
-    n=2
-    #rank2_A=[[random.randint(1,21) for x in range(n)] for y in range(n)]
-    #rank2_B=[[random.randint(1,21) for x in range(n)] for y in range(n)]
+    
+    n=3 # =10,=20
+    rank2_A=[[random.randint(1,21) for x in range(n)] for y in range(n)]
+    rank2_B=[[random.randint(1,21) for x in range(n)] for y in range(n)]
 
     rank3_A=[[[random.randint(1,5) for x in range(n)] for y in range(n)]for z in range(n)]
     rank3_B=[[[random.randint(1,5) for x in range(n)] for y in range(n)]for z in range(n)]
 
 
-    #print(rank3_B[0])
     if check_dimensions(rank3_A,rank3_B):
-        for x in rank3_A:
-             print(x)
-    print('--------------------------------')         
-    if check_dimensions(rank3_A,rank3_B):
-        for x in rank3_B:
-             print(x)
-             
-    print('--------------------------------')           
-    if check_dimensions(rank3_A,rank3_B):
-        for x in rank3_A[0]:
-             print(x)
-    print('--------------------------------')        
-    if check_dimensions(rank3_A,rank3_B):
-        for x in exctract_clmn(rank3_B,0):
-             print(x)
-    print('--------------------------------')        
-    if check_dimensions(rank3_A,rank3_B):
-        for x in rank3d(rank3_A,rank3_B):
-             print(x)   
+        Print_MAT(rank3TensorMult(rank3_A,rank3_B))
+            
