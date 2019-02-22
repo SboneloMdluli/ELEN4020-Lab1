f91dc9c84e65b1e20d39239b1791820771525b12
SboneloMdluli <1101772@students.wits.ac.za>
2019-02-22 16:19:04 +0200

    Added Tensor addition functions

--- lab1.py
+++ lab1.py
@@ -26,18 +26,21 @@ def rowvec_to_mat(vec_b):
     
     return rol_matrix
     
-def rank3TensorAdd(A,val,x,i,k):
-    A[x][i][k]+=val
+def Element3D_Add(A,val,i,j,k):
+    A[j][i][k]+=val
     
+
+def Element2D_Add(a,b,c,i,j):
+
+    for k in range(len(a[0])):
+        c[i][j] += a[i][k]*b[k][j]
     
 def rank3TensorMult(a,b):
     """Perform 3-D by 3-D array multiplication"""
-
-    h_plane = [[0 for x in range(len(a))] for y in range(len(a))]
     
     rank3_C=[[[0 for x in range(n)] for y in range(n)]for z in range(n)]
         
-    mat_a = mat_b = [[0 for x in range(len(a))] for y in range(len(a))]
+    h_plane = mat_a = mat_b = [[0 for x in range(len(a))] for y in range(len(a))]
     
     tmp=rslt_ =0
         
@@ -46,7 +49,6 @@ def rank3TensorMult(a,b):
        
         for j in range(len(b)):
             v_plane = exctract_clmn(b,j) # select vertical plane in b
-
             
             for k in range(len(h_plane)): #go through rows in horizontal plane
                 
@@ -56,23 +58,27 @@ def rank3TensorMult(a,b):
                     
                     # must convert vector to n*n matrices
                     mat_a =rowvec_to_mat(h_plane[k])
-                    
+           
                     mat_b =colvec_to_mat(Vplane_clm)
                     
-
                     tmp=rank2TensorMult(mat_a,mat_b) 
                     rslt_ = tmp[0][0] # get single value
                     
-                    rank3TensorAdd(rank3_C,rslt_,x,i,k)
+                    Element3D_Add(rank3_C,rslt_,x,i,k)
                    
 
     return rank3_C
+    
+def rank3TensorAdd(A,B):
+    """Perform 3-D by 3-D array addition"""
+    
+    add_C=[[[0 for x in range(len(A))] for y in range(len(A))]for z in range(len(A))]
 
-def rank2TensorAdd(a,b,c,i,j):
-
-    for k in range(len(a[0])):
-        c[i][j] += a[i][k]*b[k][j]
-
+    for i in range(len(A)): # iterate through rows
+    
+        add_C[i] = rank2TensorAdd(A[i],B[i])
+    
+    return add_C
 
 def rank2TensorMult(a,b):
     """Perform 2-D by 2-D array multiplication"""
@@ -81,10 +87,22 @@ def rank2TensorMult(a,b):
     for i in range(len(a)): # iterate through rows
         for j in range(len(b[0])): # iterate through columns
 
-            rank2TensorAdd(a,b,c,i,j)
+            Element2D_Add(a,b,c,i,j)
 
     return c
+    
+    
+def rank2TensorAdd(a,b):
+    """Perform 2-D by 2-D array addition"""
+    add_c = [[0 for x in range(len(a))] for y in range(len(b))] 
 
+    for i in range(len(a)): # iterate through rows
+        for j in range(len(b)): # iterate through columns
+
+            add_c [i][j] = a[i][j] + b [i][j]
+
+    return add_c
+        
 
 def check_dimensions(a,b):
     """Check if dimensiosn are compatible for matrix multiplication"""
@@ -97,19 +115,27 @@ def check_dimensions(a,b):
 #main program 
 if __name__ == "__main__":
     
-    n=3 # =10,=20
+    n=2 # =10,=20
     rank2_A=[[random.randint(1,21) for x in range(n)] for y in range(n)]
     rank2_B=[[random.randint(1,21) for x in range(n)] for y in range(n)]
 
     rank3_A=[[[random.randint(1,21) for x in range(n)] for y in range(n)]for z in range(n)]
     rank3_B=[[[random.randint(1,21) for x in range(n)] for y in range(n)]for z in range(n)]
-
-    print('-----------------------------RANK2-TENSOR-----------------------------')
+    
+  
+    print('-----------------------------RANK2-TENSOR-MULTIPLICATION----------------------------')
+    
     if check_dimensions(rank2_A,rank2_B):
         Print_MAT(rank2TensorMult(rank2_A,rank2_B))
-   
-    print('-----------------------------RANK3-TENSOR-----------------------------')
+        print('-----------------------------RANK2-TENSOR-ADDITION-----------------------------')
+        x =rank2TensorMult(rank2_A,rank2_B)
+        Print_MAT(rank2TensorAdd(x,x))
+        
+        
+    print('-----------------------------RANK3-TENSOR-MULTIPLICATION----------------------------')
     
     if check_dimensions(rank3_A,rank3_B):
-        Print_MAT(rank3TensorMult(rank3_A,rank3_B))
-            
+        Print_MAT(rank3TensorMult(rank3_A,rank3_B))  
+        print('-----------------------------RANK3-TENSOR-ADDITION-----------------------------')
+        y =rank3TensorMult(rank3_A,rank3_B)
+        Print_MAT(rank3TensorAdd(y,y))  
